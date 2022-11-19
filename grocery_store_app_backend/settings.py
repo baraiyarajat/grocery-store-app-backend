b1c04234379557a44e5de3a6914259e22453d4e7
Rajat Baraiya <baraiya.rajat@gmail.com>
2022-11-19 19:36:14 +0000

    Moved corsheaders middleware to the first position in the MIDDLEWARE list

--- grocery_store_app_backend/settings.py
+++ grocery_store_app_backend/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+
 ]
 
 REST_FRAMEWORK = {
