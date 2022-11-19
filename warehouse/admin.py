0ac3ba8807bd04c9f0b5b9d9be9259284f21d9c1
Rajat Baraiya <baraiya.rajat@gmail.com>
2022-11-19 10:44:45 +0000

    Added warehouse model

--- warehouse/admin.py
+++ warehouse/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Warehouse
 
-# Register your models here.
+
+class WarehouseAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Warehouse, WarehouseAdmin)
--- warehouse/models.py
+++ warehouse/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Warehouse(models.Model):
+    name = models.CharField(max_length=200, null=False, unique=True)
+    slug = models.SlugField(max_length=200, blank=True, null=True)
+    default_selected = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
