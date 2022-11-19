7ca312682a76efa9a3f1cc9d8f1ed47d6c341a9c
Rajat Baraiya <baraiya.rajat@gmail.com>
2022-11-19 13:32:28 +0000

    Removed username field from Account admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,7 +4,7 @@ from .models import Account
 
 
 class AccountAdmin(UserAdmin):
-    list_display = ('email', 'first_name', 'last_name', 'username', 'last_login', 'date_joined', 'is_active')
+    list_display = ('email', 'first_name', 'last_name', 'last_login', 'date_joined', 'is_active')
     list_display_links = ('email', 'first_name', 'last_name')
     readonly_fields = ('date_joined', 'last_login')
     ordering = ('-date_joined',)
