701218b5d3ea9b169fdecc77c0c4e1b9bc8acd53
Rajat Baraiya <baraiya.rajat@gmail.com>
2022-11-19 23:17:10 +0000

    Registered Category model

--- category/admin.py
+++ category/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Category
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Category, CategoryAdmin)
