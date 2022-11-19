ded02e57ab88d9dc2e0162998ef8a423861fa9b4
Rajat Baraiya <baraiya.rajat@gmail.com>
2022-11-19 23:20:25 +0000

    Created category_list and category_details view

--- category/views.py
+++ category/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from .models import Category
+from rest_framework.decorators import api_view
+from .serializers import CategorySerializer
+from django.http.response import JsonResponse
 
-# Create your views here.
+
+@api_view(['GET', ])
+def category_list(request):
+    if request.method == 'GET':
+        categories = Category.objects.all()
+
+        category_serializer = CategorySerializer(categories, many=True)
+        return JsonResponse(category_serializer.data, safe=False)
+
+
+@api_view(['GET', ])
+def category_details(request, category_id):
+    if request.method == 'GET':
+        category = Category.objects.get(id=category_id)
+
+        category_serializer = CategorySerializer(category)
+        return JsonResponse(category_serializer.data, safe=False)
