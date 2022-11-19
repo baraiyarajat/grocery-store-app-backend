0af19aeb5a117cd34a9a2898d61d743c94601a29
Rajat Baraiya <baraiya.rajat@gmail.com>
2022-11-19 10:44:20 +0000

    Added accounts model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Account
 
-# Register your models here.
+
+class AccountAdmin(UserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'username', 'last_login', 'date_joined', 'is_active')
+    list_display_links = ('email', 'first_name', 'last_name')
+    readonly_fields = ('date_joined', 'last_login')
+    ordering = ('-date_joined',)
+
+    # Other parameters required for custom user model
+    list_filter = ()
+    filter_horizontal = ()
+    fieldsets = ()
+
+
+# Model Registration
+admin.site.register(Account, AccountAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,78 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+
+class AccountManager(BaseUserManager):
+
+    def create_user(self, first_name, last_name, username, email, password=None):
+        if not email:
+            raise ValueError('Email Address cannot be empty')
+        if not username:
+            raise ValueError('User name cannot be empty')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+            first_name=first_name,
+            last_name=last_name
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, first_name, last_name, username, email, password):
+        # Create normal user
+        user = self.create_user(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            password=password,
+            email=self.normalize_email(email))
+
+        user.is_admin = True
+        user.is_active = True
+        user.is_staff = True
+        user.is_superadmin = True
+
+        user.save(using=self._db)
+
+        return user
+
+
+class Account(AbstractBaseUser):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    phone_number = models.CharField(max_length=50)
+    profile_picture = models.ImageField(upload_to='photos/profile_pictures', blank=True,
+                                        default='photos/defaults/user_avatar.png')
+
+    # Required for custom user model
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now_add=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_superadmin = models.BooleanField(default=False)
+
+    # To check if the account is for customer
+    is_customer = models.BooleanField(default=False)
+
+    # Allows users to log in with email address
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    objects = AccountManager()
+
+    def __str__(self):
+        return self.email
+
+    # Admin has the required permissions
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, add_label):
+        return True
