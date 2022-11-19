156570e7fcaafbd9dace0950a75923af81217467
Rajat Baraiya <baraiya.rajat@gmail.com>
2022-11-19 21:01:50 +0000

    Added warehouse_list and warehouse_detail views

--- warehouse/views.py
+++ warehouse/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from .models import Warehouse
+from rest_framework.decorators import api_view
+from .serializers import WarehouseSerializer
+from django.http.response import JsonResponse
 
-# Create your views here.
+
+@api_view(['GET', ])
+def warehouse_list(request):
+    if request.method == 'GET':
+        warehouses = Warehouse.objects.all()
+
+        warehouse_serializer = WarehouseSerializer(warehouses, many=True)
+        return JsonResponse(warehouse_serializer.data, safe=False)
+
+
+@api_view(['GET', ])
+def warehouse_details(request, warehouse_id):
+    if request.method == 'GET':
+        warehouse = Warehouse.objects.get(id=warehouse_id)
+
+        warehouse_serializer = WarehouseSerializer(warehouse)
+        return JsonResponse(warehouse_serializer.data, safe=False)
