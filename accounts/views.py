e5123729885fc7b5570c574373814e8d3a83a0c9
Rajat Baraiya <baraiya.rajat@gmail.com>
2022-11-19 13:33:52 +0000

    Added AccountCreate View

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import AccountSerializer
+from .models import Account
 
-# Create your views here.
+
+class AccountCreate(APIView):
+    """
+    Creates the Account.
+    """
+
+    def post(self, request, format='json'):
+        serializer = AccountSerializer(data=request.data)
+        if serializer.is_valid():
+            account = serializer.save()
+            if account:
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
