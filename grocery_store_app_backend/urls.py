2db4243002d34fe63a109721d1b09983dde95895
Rajat Baraiya <baraiya.rajat@gmail.com>
2022-11-25 18:49:57 +0000

    included warehouse and address urls

--- grocery_store_app_backend/urls.py
+++ grocery_store_app_backend/urls.py
@@ -8,6 +8,7 @@ from django.conf.urls.static import static
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('accounts/', include('accounts.urls')),
-                  path('v0/warehouses/', include('warehouse.urls')),
+                  path('api/v0/warehouses/', include('warehouse.urls')),
+                  path('api/v0/addresses/', include('address.urls')),
                   path('v0/categories/', include('category.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
