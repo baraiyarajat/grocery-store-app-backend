b0571c322d9338c0015161f2a1f7c469903371de
Rajat Baraiya <baraiya.rajat@gmail.com>
2022-11-25 18:50:57 +0000

    Added selected-warehouse url

--- warehouse/urls.py
+++ warehouse/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.warehouse_list, name='warehouse_list'),
     path('<int:warehouse_id>', views.warehouse_details, name='warehouse_details'),
+    path('selected-warehouse', views.SelectedWarehouseViewAPI.as_view(), name='selected_warehouse'),
 ]
