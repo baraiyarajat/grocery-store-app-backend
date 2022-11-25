765957f0edbd97a7633a1c5292b03866e25b585a
Rajat Baraiya <baraiya.rajat@gmail.com>
2022-11-25 18:50:35 +0000

    Created SelectedWarehouseSerializer

--- warehouse/serializers.py
+++ warehouse/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
-from .models import Warehouse
+from .models import Warehouse, SelectedWarehouse
 
 
 class WarehouseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Warehouse
         fields = ('id', 'name', 'slug')
+
+
+class SelectedWarehouseSerializer(serializers.ModelSerializer):
+    warehouse = WarehouseSerializer(read_only=True)
+
+    class Meta:
+        model = SelectedWarehouse
+        fields = ('warehouse',)
