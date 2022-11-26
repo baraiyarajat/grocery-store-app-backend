f50af1a1f70e8ea7ec35cff1289a792663079b32
Rajat Baraiya <baraiya.rajat@gmail.com>
2022-11-26 15:11:51 +0000

    Created WarehouseProductListView APIView

--- warehouse_product/views.py
+++ warehouse_product/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import WarehouseProduct
+from category.models import Category
+from .serializers import WarehouseProductSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+class WarehouseProductListView(APIView):
+
+    def get(self, request):
+        category_slug = request.GET['category_slug']
+        warehouse_id = request.GET['warehouse_id']
+        category_object = Category.objects.get(slug=category_slug)
+
+        warehouse_product_objects = WarehouseProduct.objects.filter(product__category=category_object,
+                                                                    warehouse__id=warehouse_id)
+        warehouse_product_serializer = WarehouseProductSerializer(warehouse_product_objects, many=True)
+        return Response(warehouse_product_serializer.data, status=200)
