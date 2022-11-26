3b6beec49dc5c42fd33692a34c5fca47c176fd65
Rajat Baraiya <baraiya.rajat@gmail.com>
2022-11-26 15:10:00 +0000

    Created WarehouseProduct model

--- warehouse_product/models.py
+++ warehouse_product/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from warehouse.models import Warehouse
+from product.models import Product
 
-# Create your models here.
+
+class WarehouseProduct(models.Model):
+    warehouse = models.ForeignKey(Warehouse, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.IntegerField()
+    stock = models.IntegerField()
+    discount_rate = models.IntegerField(default=0)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = (('warehouse', 'product'),)
+
+    def __str__(self):
+        return self.product.name
+
+    def get_discounted_price(self):
+        return self.price - (self.price * self.discount_rate / 100)
