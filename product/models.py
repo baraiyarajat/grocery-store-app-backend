a34317ca1aa61f251746f72aad7b8d38cd08fa45
Rajat Baraiya <baraiya.rajat@gmail.com>
2022-11-26 13:21:52 +0000

    Added Product model

--- product/models.py
+++ product/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from category.models import Category
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=400, unique=True)
+    slug = models.SlugField(max_length=400, blank=True, null=True)
+    description = models.CharField(max_length=2000, blank=True, null=True)
+    benefits = models.CharField(max_length=2000, blank=True, null=True)
+    how_to_use = models.CharField(max_length=2000, blank=True, null=True)
+    disclaimer = models.CharField(max_length=2000, blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(max_length=255, null=True, upload_to='photos/product_primary_image')
+
+    def __str__(self):
+        return self.name
