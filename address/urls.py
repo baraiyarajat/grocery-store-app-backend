58d6bfbab25813593d2ffdf85bf850f38aaa23b8
Rajat Baraiya <baraiya.rajat@gmail.com>
2022-11-26 13:08:23 +0000

    Added single address url

--- address/urls.py
+++ address/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.AddressListView.as_view(), name='address_list'),
+    path('<int:address_id>', views.AddressDetailView.as_view(), name='address_detail')
 ]
