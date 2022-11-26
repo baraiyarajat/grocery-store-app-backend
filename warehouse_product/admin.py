eaaa796914ed57062b754fdc7ae16df7b93baebb
Rajat Baraiya <baraiya.rajat@gmail.com>
2022-11-26 15:12:21 +0000

    Registered WarehouseProduct model to adminsite

--- warehouse_product/admin.py
+++ warehouse_product/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import WarehouseProduct
 
-# Register your models here.
+
+class WarehouseProductAdmin(admin.ModelAdmin):
+    list_display = ('product', 'warehouse', 'stock', 'price', 'modified_date')
+
+
+admin.site.register(WarehouseProduct, WarehouseProductAdmin)
