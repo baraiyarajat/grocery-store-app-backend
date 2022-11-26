974d6be515373d2a09316c7e6f8fc383a4aae59a
Rajat Baraiya <baraiya.rajat@gmail.com>
2022-11-26 13:22:15 +0000

    Regsitered product model in admin site

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Product, ProductAdmin)
+
