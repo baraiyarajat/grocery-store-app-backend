66e9bb42db6c22dbfd8c1e67fb067ff88e3a12f8
Rajat Baraiya <baraiya.rajat@gmail.com>
2022-12-02 05:00:20 +0000

    Added new_warehouse_product_list and warehouse_product_details urls

--- warehouse_product/urls.py
+++ warehouse_product/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.WarehouseProductListView.as_view(), name='warehouse_product_list'),
+    path('newly-added-products', views.NewWarehouseProducts.as_view(), name='new_warehouse_product_list'),
+    path('<slug:product_slug>', views.WarehouseProductDetailsView.as_view(), name='warehouse_product_details'),
 ]
