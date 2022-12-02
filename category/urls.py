b5bb62c4bc3c066c8c65ed6a3bf0c6c566761b94
Rajat Baraiya <baraiya.rajat@gmail.com>
2022-12-02 04:56:14 +0000

    Added category_details_by_slug url

--- category/urls.py
+++ category/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.category_list, name='category_list'),
     path('<int:category_id>', views.category_details, name='category_details'),
+    path('<slug:category_slug>', views.category_details_by_slug, name='category_details_by_slug'),
 ]
