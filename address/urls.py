fa6cd57d36912a711c3480880fde49d2051f84b3
Rajat Baraiya <baraiya.rajat@gmail.com>
2022-12-02 04:55:53 +0000

    Added 'add-address' and 'edit-address' urls

--- address/urls.py
+++ address/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.AddressListView.as_view(), name='address_list'),
+    path('add-address', views.AddAddressView.as_view(), name='add_address'),
+    path('edit-address', views.EditAddressView.as_view(),name='edit_address'),
     path('<int:address_id>', views.AddressDetailView.as_view(), name='address_detail')
 ]
