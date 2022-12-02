5c00b9cf550d69e2ff3eff915489a5b3706bfee2
Rajat Baraiya <baraiya.rajat@gmail.com>
2022-12-02 17:45:05 +0000

    Added delete_cart_item url

--- cart/urls.py
+++ cart/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.CartProductsListView.as_view(), name='cart_products'),
+    path('delete/<int:cart_item_id>', views.DeleteCartProduct.as_view(),name='delete_cart_item')
 
 ]
