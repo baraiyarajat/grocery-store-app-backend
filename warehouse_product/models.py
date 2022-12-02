efbddb9a97702235bd9ebc81d7c1588f7dc38907
Rajat Baraiya <baraiya.rajat@gmail.com>
2022-12-02 04:52:07 +0000

    modified __str__ and get_discounted_price values

--- warehouse_product/models.py
+++ warehouse_product/models.py
@@ -12,11 +12,12 @@ class WarehouseProduct(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
 
+
     class Meta:
         unique_together = (('warehouse', 'product'),)
 
     def __str__(self):
-        return self.product.name
+        return "{}/{}".format(self.warehouse.name,self.product.name)
 
     def get_discounted_price(self):
-        return self.price - (self.price * self.discount_rate / 100)
+        return round(self.price - (self.price * self.discount_rate / 100), 2)
