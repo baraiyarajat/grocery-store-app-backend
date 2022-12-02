7e17ba2e48a4daaf300582464025e64b5b5d0c9b
Rajat Baraiya <baraiya.rajat@gmail.com>
2022-12-02 04:59:06 +0000

    Added WarehouseProductDetailsView and NewWarehouseProducts views

--- warehouse_product/views.py
+++ warehouse_product/views.py
@@ -5,6 +5,7 @@ from category.models import Category
 from .serializers import WarehouseProductSerializer
 from rest_framework.response import Response
 
+
 class WarehouseProductListView(APIView):
 
     def get(self, request):
@@ -13,6 +14,24 @@ class WarehouseProductListView(APIView):
         category_object = Category.objects.get(slug=category_slug)
 
         warehouse_product_objects = WarehouseProduct.objects.filter(product__category=category_object,
-                                                                    warehouse__id=warehouse_id)
+                                                                    warehouse__id=warehouse_id).order_by('product__name')
         warehouse_product_serializer = WarehouseProductSerializer(warehouse_product_objects, many=True)
         return Response(warehouse_product_serializer.data, status=200)
+
+
+class WarehouseProductDetailsView(APIView):
+
+    def get(self, request, product_slug):
+        warehouse_id = request.GET["warehouse_id"]
+        warehouse_product_object = WarehouseProduct.objects.get(product__slug=product_slug, warehouse__id=warehouse_id)
+        warehouse_product_serializer = WarehouseProductSerializer(warehouse_product_object)
+        return Response(warehouse_product_serializer.data, status=200)
+
+
+class NewWarehouseProducts(APIView):
+
+    def get(self, request):
+        warehouse_id = request.GET['warehouse_id']
+        new_warehouse_product_objects = WarehouseProduct.objects.filter(warehouse__id=warehouse_id).order_by('-modified_date')[:25]
+        new_warehouse_product_serializer = WarehouseProductSerializer(new_warehouse_product_objects, many=True)
+        return Response(new_warehouse_product_serializer.data, status=200)
