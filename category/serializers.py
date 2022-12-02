85ffddc511a7f32b9c0cb00458550f368b3ad046
Rajat Baraiya <baraiya.rajat@gmail.com>
2022-12-02 04:53:00 +0000

    Added image field to CategorySerializer

--- category/serializers.py
+++ category/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 from .models import Category
-
+from django.conf import settings
 
 class CategorySerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField('get_image_url')
+
     class Meta:
         model = Category
         fields = ('id', 'name', 'slug', 'image')
+        # fields = ('id', 'name', 'slug', 'category_image')
+
+    def get_image_url(self, obj):
+        return   obj.image.url
