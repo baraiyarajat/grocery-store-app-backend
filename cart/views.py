d0e7b102e63ebdaca2dbb4acd06e006c817cd9ca
Rajat Baraiya <baraiya.rajat@gmail.com>
2022-12-02 05:31:20 +0000

    Added CartProductsListView

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from accounts.models import Account
+from .models import CartProduct
+from .serializers import CartProductSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+
+class CartProductsListView(APIView):
+    def get(self, request):
+        try:
+            user_id = request.GET['user_id']
+            user_object = Account.objects.get(id=user_id)
+            cart_product_objects = CartProduct.objects.filter(user=user_object)
+            cart_products_serializer = CartProductSerializer(cart_product_objects, many=True)
+            return Response(cart_products_serializer.data, status=200)
+        except Exception as e:
+            print(e)
+            return Response(status=500)
