52aa78de8cd288b536ede8f495cdee0b5c283eb3
Rajat Baraiya <baraiya.rajat@gmail.com>
2022-12-02 04:50:46 +0000

    Created WishlistProduct model

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from accounts.models import Account
+from warehouse_product.models import WarehouseProduct
 
-# Create your models here.
+
+class WishlistProduct(models.Model):
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    warehouse_product = models.ForeignKey(WarehouseProduct, on_delete=models.CASCADE)
+    date_added = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = (('user', 'warehouse_product'),)
+
+    def __str__(self):
+        return self.warehouse_product.product.name
