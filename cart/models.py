50321f2653c2fef1e958183f9d39a5b6d7e30c51
Rajat Baraiya <baraiya.rajat@gmail.com>
2022-12-03 00:17:08 +0000

    Modified CartProduct model

--- cart/models.py
+++ cart/models.py
@@ -1,14 +1,19 @@
 from django.db import models
 from accounts.models import Account
 from warehouse_product.models import WarehouseProduct
+from warehouse.models import Warehouse
 
 
 class CartProduct(models.Model):
     user = models.ForeignKey(Account, on_delete=models.CASCADE)
     warehouse_product = models.ForeignKey(WarehouseProduct, on_delete=models.CASCADE)
+    warehouse = models.ForeignKey(Warehouse, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = (('user', 'warehouse_product'),)
 
     def __str__(self):
         return self.warehouse_product.product.name
