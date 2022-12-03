0be24f021c636ce8cd9b8a659fa6d618d695eaad
Rajat Baraiya <baraiya.rajat@gmail.com>
2022-12-03 00:19:19 +0000

    Modified decrease_cart_item_quantity url

--- cart/urls.py
+++ cart/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('delete/<int:cart_item_id>', views.DeleteCartProduct.as_view(), name='delete_cart_item'),
     path('add', views.AddCartProduct.as_view(), name='add_cart_item'),
     path('increase-quantity', views.IncreaseCartProductQuantity.as_view(), name='increase_cart_item_quantity'),
-    path('decrease-quantity', views.DeleteCartProductQuantity.as_view(), name='decrease_cart_item_quantity'),
+    path('decrease-quantity', views.DecreaseCartProductQuantity.as_view(), name='decrease_cart_item_quantity'),
 
 ]
