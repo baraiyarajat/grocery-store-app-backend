a725ffcda3ac62ebbd84fec36a6d343b9fe8c4aa
Rajat Baraiya <baraiya.rajat@gmail.com>
2022-12-03 00:17:58 +0000

    Added add_cart_item, increase_cart_item_quantity and delete_cart_item_quantity urls

--- cart/urls.py
+++ cart/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.CartProductsListView.as_view(), name='cart_products'),
-    path('delete/<int:cart_item_id>', views.DeleteCartProduct.as_view(),name='delete_cart_item')
+    path('delete/<int:cart_item_id>', views.DeleteCartProduct.as_view(), name='delete_cart_item'),
+    path('add', views.AddCartProduct.as_view(), name='add_cart_item'),
+    path('increase-quantity', views.IncreaseCartProductQuantity.as_view(), name='increase_cart_item_quantity'),
+    path('decrease-quantity', views.DeleteCartProductQuantity.as_view(), name='decrease_cart_item_quantity'),
 
 ]
