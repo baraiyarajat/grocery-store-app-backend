048452ef5bd2a959a77819e62ec302fe78497897
Rajat Baraiya <baraiya.rajat@gmail.com>
2022-12-08 06:07:43 +0000

    Added formatted_modified_date method

--- wallet/models.py
+++ wallet/models.py
@@ -8,3 +8,5 @@ class Wallet(models.Model):
     cashback_balance = models.DecimalField(default=0, decimal_places=2, max_digits=5)
     modified_date = models.DateTimeField(auto_now=True)
 
+    def formatted_modified_date(self):
+        return self.modified_date.strftime("%b %d %Y")
