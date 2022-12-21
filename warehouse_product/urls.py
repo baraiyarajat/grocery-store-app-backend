67c653a58efc5797fa36bc16d95bcbd941fc5a8a
Rajat Baraiya <baraiya.rajat@gmail.com>
2022-12-21 17:45:41 +0000

    Added feature-products and search-products urls

--- warehouse_product/urls.py
+++ warehouse_product/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.WarehouseProductListView.as_view(), name='warehouse_product_list'),
     path('newly-added-products', views.NewWarehouseProducts.as_view(), name='new_warehouse_product_list'),
+    path('featured-products', views.FeaturedProducts.as_view(), name='featured_product_list'),
+    path('search-products', views.SearchProducts.as_view(), name='search_product_list'),
     path('<slug:product_slug>', views.WarehouseProductDetailsView.as_view(), name='warehouse_product_details'),
 ]
