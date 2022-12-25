83f884920bbd48007194cad0010b7e163382c380
Rajat Baraiya <baraiya.rajat@gmail.com>
2022-12-25 16:48:38 +0000

    Added warehouse serializer object for city field

--- address/serializers.py
+++ address/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
 from .models import Address
-
+from warehouse.serializers import WarehouseSerializer
 
 class AddressSerializer(serializers.ModelSerializer):
 
+    city = WarehouseSerializer()
     class Meta:
         model = Address
         fields = ('id', 'address_type', 'address_line_1', 'address_line_2', 'pincode', 'city')
