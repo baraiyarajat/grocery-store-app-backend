3b5285b7bac8f50fb8027cd2fe83f8f38fe2af96
Rajat Baraiya <baraiya.rajat@gmail.com>
2023-01-05 21:17:38 +0000

    Modified WalletDetailsView

--- wallet/views.py
+++ wallet/views.py
@@ -4,12 +4,20 @@ from accounts.models import Account
 from .models import Wallet
 from .serializers import WalletSerializer
 from rest_framework.response import Response
-
+from accounts.authentication import JWTAuthentication
+from django.core.exceptions import ObjectDoesNotExist
 
 class WalletDetailsView(APIView):
+    authentication_classes = [JWTAuthentication, ]
     def get(self, request, user_id):
         try:
             wallet_object = Wallet.objects.get(user__id=user_id)
+            wallet_serializer = WalletSerializer(wallet_object)
+            return Response(wallet_serializer.data, status=200)
+        except ObjectDoesNotExist:
+            print('Creating new Wallet')
+            user = Account.objects.get(id=user_id)
+            wallet_object = Wallet.objects.create(user=user)
             wallet_serializer = WalletSerializer(wallet_object)
             return Response(wallet_serializer.data, status=200)
         except Exception as e:
