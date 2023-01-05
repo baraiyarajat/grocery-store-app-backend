09148fff45d1cea8ab2de2ea535236f27d0e923c
Rajat Baraiya <baraiya.rajat@gmail.com>
2023-01-05 21:15:35 +0000

    -Changed is_active to true by default

--- accounts/models.py
+++ accounts/models.py
@@ -50,7 +50,8 @@ class Account(AbstractBaseUser):
     last_login = models.DateTimeField(auto_now_add=True)
     is_admin = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=False)
+    # is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     is_superadmin = models.BooleanField(default=False)
 
     # To check if the account is for customer
